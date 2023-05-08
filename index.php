79b54e9ec2219a10ea5585c8c89f4faef31d85d8
Grzegorz Pasich <grzegorz.pasich@o2.pl>
2023-05-08 21:54:54 +0200

    first stage

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Routing::get('', 'DefaultController');
+Routing::get('homepage', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::get('articles', 'DefaultController');
+Routing::get('diets', 'DefaultController');
+Routing::get('bmiCalculator', 'DefaultController');
+Routing::get('information', 'DefaultController');
+
+Routing::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function homepage()
+    {
+        $this->render('HomePage');
+    }
+
+    public function login()
+    {
+        $this->render('Login');
+    }
+
+    public function articles()
+    {
+        $this->render('Articles');
+    }
+
+    public function diets()
+    {
+        $this->render('Diets');
+    }
+
+    public function bmiCalculator()
+    {
+        $this->render('BmiCalculator');
+    }
+
+    public function information()
+    {
+        $this->render('Information');
+    }
+}
