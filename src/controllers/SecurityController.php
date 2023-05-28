eedff3587ee4c2d8a6656b9776ac63c3299132e9
Grzegorz Pasich <grzegorz.pasich@o2.pl>
2023-05-28 11:42:41 +0200

    login and read data

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,11 +2,12 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController {
     public function login() {
-        $user = new User('grzegorz.pasich@o2.pl', 'admin', 'Grzesiek', 'Pasich');
-
+        
+        $userRepository = new UserRepository();
 
         if(!$this->isPost()) {
             return $this->render('LoginPage');
@@ -15,12 +16,18 @@ class SecurityController extends AppController {
         $email = $_POST["email"];
         $password = $_POST["password"];
 
+        $user = $userRepository->getUser($email);
+
+        if(!$user) {
+            return $this->render('LoginPage', ['messages' => ['User not found!']]);
+        }
+
         if ($user->getEmail() !== $email) {
-            return $this->render('LoginPage', ['messages' => ['User with this email not exist !']]);
+            return $this->render('LoginPage', ['messages' => ['User with this email not exist!']]);
         }
 
         if ($user->getPassword() !== $password) {
-            return $this->render('LoginPage', ['messages' => ['Wrong password !']]);
+            return $this->render('LoginPage', ['messages' => ['Wrong password!']]);
         }
 
         return $this->render('HomePage');
